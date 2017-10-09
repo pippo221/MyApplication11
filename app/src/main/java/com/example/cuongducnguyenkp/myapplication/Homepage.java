fe271d04cefc378c02b62709cf2b214ceeeb8ba5
cuongducnguyen.kp <lumo.aq@gmail.com>
2017-10-09 17:15:37 +0700

    abc

---
+++ app/src/main/java/com/example/cuongducnguyenkp/myapplication/Homepage.java
@@ -0,0 +1,15 @@
+package com.example.cuongducnguyenkp.myapplication;
+
+import android.os.Bundle;
+
+/**
+ * Created by cuongducnguyen.kp on 10/9/2017.
+ */
+
+public class Homepage extends LoginActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_view_customer);
+    }
+}
